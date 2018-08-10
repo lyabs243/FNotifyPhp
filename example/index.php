2775c510f90218f45276011cbb7cf6cd66fee714
test <loicyabili@gmail.com>
2018-08-10 15:46:04 +0200

    Example Page

---
+++ example/index.php
@@ -0,0 +1,14 @@
+<?php
+include_once './lib/FNotifyPhp.php';
+//initialize config file
+$configs = include('./lib/config/conf.php');
+$registrationIds = 'Registration ID';
+$title = 'My title';
+$message = 'The Message';
+
+$fNotifyPhp = new FNotifyPhp($configs);
+$fNotifyPhp->setBody($message);
+$fNotifyPhp->setTo($registrationIds);
+$fNotifyPhp->setTitle($title);
+$result = $fNotifyPhp->notifyRecipient();
+echo $result;
